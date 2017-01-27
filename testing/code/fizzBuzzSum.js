c767e55ec5c142fce1f1be18faceb1c003135d96
Allon Hadaya <self@allon.nyc>
2017-01-26 19:37:55 -0500

    Add an arithmetic solution.

--- testing/code/fizzBuzzSum.js
+++ testing/code/fizzBuzzSum.js
@@ -1,5 +1,13 @@
-const fizzBuzzSum = n => {
-  // make the tests pass
-};
+// http://www.mathsisfun.com/algebra/sequences-sums-arithmetic.html
+// where a = n, simplify
+const arithmeticSum = (n, d) => (n / 2) * (n + 1) * d;
+
+const sumMultiplesUntil = (m, n) => arithmeticSum(Math.floor(n / m), m);
+
+const fizzBuzzSum = n =>
+  // add up the 3s and 5s
+  sumMultiplesUntil(3, n) + sumMultiplesUntil(5, n) -
+  // but only count the 15s once
+  sumMultiplesUntil(15, n);
 
 module.exports = { fizzBuzzSum };
