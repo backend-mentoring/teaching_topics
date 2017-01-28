f086eb2cf87ddd4018be1974ce66d394b8c47bd3
Vincent Trivett <trivett@gmail.com>
2017-01-28 09:27:36 -0500

    maybe do more than one geometry related function

--- testing/write-the-tests/geometry.js
+++ testing/write-the-tests/geometry.js
@@ -1,5 +1,15 @@
-const geometry = () =>{
+const geometry = {
+  isTriangle: () => {
+    console.log("called");
+  },
 
-}
+  isCircle: () => {
+    console.log('called');
+  }
+
+};
+
+
+
 
 module.exports = geometry;
