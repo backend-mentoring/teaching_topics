6c1a2a679622aaf9f0a0ae461a19db1a2e47bec4
Vincent Trivett <trivett@gmail.com>
2017-02-09 07:43:14 -0500

    eschaton!

--- testing/make-a-feature-with-tests/eschaton.js
+++ testing/make-a-feature-with-tests/eschaton.js
@@ -3,7 +3,7 @@ const eschaton = {
     // assuming an object as defender and attacker with a strength attribute,
     // depending on who is stronger, defender or attacker wins
     // after that passes, include a shield attribute that helps decide the winner
-    // go cray!
+    //
 
     //
   }
--- testing/make-a-feature-with-tests/test/eschatonTest.js
+++ testing/make-a-feature-with-tests/test/eschatonTest.js
@@ -1,10 +1,21 @@
 const { assert, expect } = require('chai');
 
-const geometry = require('../eschaton');
+const eschaton = require('../eschaton');
 
 describe('eschaton', () => {
   it('should exist', () => {
     expect(eschaton).to.exist;
   });
+
+  // write a test to make sure that the stronger beligerent wins
+
+  // then write the code to make it pass
+
+  // then add shields as a thing and make the old test still pass
+
+  // keep making it more complex with your tests guiding you.
+
+  // just get in those reps!
+
 
 });
