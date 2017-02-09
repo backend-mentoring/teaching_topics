3a0b193c9fecf5eb19ce714fa20574ccbd3f5f46
Vincent Trivett <trivett@gmail.com>
2017-02-08 22:11:28 -0500

    eschaton!

---
+++ testing/make-a-feature-with-tests/eschaton.js
@@ -0,0 +1,12 @@
+const eschaton = {
+  fight: (defender, attacker) => {
+    // assuming an object as defender and attacker with a strength attribute,
+    // depending on who is stronger, defender or attacker wins
+    // after that passes, include a shield attribute that helps decide the winner
+    // go cray!
+
+    //
+  }
+};
+
+module.exports = eschaton;
---
+++ testing/make-a-feature-with-tests/test/eschatonTest.js
@@ -0,0 +1,10 @@
+const { assert, expect } = require('chai');
+
+const geometry = require('../eschaton');
+
+describe('eschaton', () => {
+  it('should exist', () => {
+    expect(eschaton).to.exist;
+  });
+
+});
