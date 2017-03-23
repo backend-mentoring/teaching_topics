8ce2c311ba810ee6a0d543a0a7752dab28b8806a
Giulia <giulialake@gmail.com>
2017-03-23 17:47:52 -0400

    Fix the creation of the friendships table.

--- sql/orm/models.js
+++ sql/orm/models.js
@@ -15,21 +15,24 @@ CREATE TABLE friendships (
   id INTEGER PRIMARY KEY ASC,
   person_a INTEGER,
   person_b INTEGER,
-  FOREIGN KEY(person_a, person_b) REFERENCES people(id),
-  UNIQUE(person_a, person_b),
+  FOREIGN KEY(person_a, person_b) REFERENCES people(id, id),
   CHECK (person_a < person_b)
-);`;
+);
+CREATE UNIQUE INDEX distinct_friends ON friendships (person_a, person_b);`;
 
 // callback will receive the Person and Friendship class.
 module.exports = function (callback) {
 
-  // create the tables, and then define the models.
-  db.run(createTablePeople + createTableFriendships, defineModels);
+    // create the tables, and then define the models.
+    db.exec(createTablePeople + createTableFriendships, defineModels);
 
-  function defineModels() {
+    function defineModels(err) {
 
     class Person {
     }
+        if (err !== null) {
+            throw err;
+        }
 
     class Friendship {
     }
