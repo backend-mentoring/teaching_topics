cd17ed3970ea2f5bc90c249be3362d6691c96324
Vincent Trivett <trivett@gmail.com>
2017-03-30 19:52:33 -0400

    promises made, promises kept

---
+++ promises/promiseExample.js
@@ -0,0 +1,28 @@
+console.log('we are runnin...');
+
+document
+  .querySelector('body')
+  .addEventListener('click', event => {
+
+    console.log('A click, a click!');
+    console.log('Lets do something asynchronous');
+
+    fetch('http://swapi.co/api/planets/1')
+      .then(r => {
+        if (r.ok) {
+          return r.json();
+        }
+        throw new Error('not ok');
+      })
+      .then(planetData =>
+        Promise.all(planetData.films.map(link =>
+          fetch(link)
+            .then(r => r.json())
+            .then(f => f.title))))
+      .then(filmTitles => { console.log(filmTitles); })
+      .catch(err => {
+        console.log(`well, I really wanted to see some starwars films, but I guess I'll just have to not... :( ${err}`);
+      })
+  });
+
+console.log('we are really runnin...')
